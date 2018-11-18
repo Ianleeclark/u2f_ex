3b3dab488a60542dc23701072ca92335f745904e
Ian Clark <ian@ianleeclark.com>
2018-11-17 18:33:02 -0800

    Cleaning up type signatures in registration request

--- lib/models/registration_request.ex
+++ lib/models/registration_request.ex
@@ -3,6 +3,11 @@ defmodule U2FEx.RegistrationRequest do
   Represents an outgoing registration request. The U2F device will take this and then be prompted to respond.
   """
 
+  @type t :: %__MODULE__{
+          challenge: binary(),
+          app_id: binary()
+        }
+
   alias U2FEx.Utils.Crypto
 
   @required_keys [:challenge, :app_id]
@@ -11,18 +16,19 @@ defmodule U2FEx.RegistrationRequest do
   @doc """
   Creates a new RegistrationRequest.
   """
-  @spec new(challenge :: String.t(), app_id :: String.t()) :: %__MODULE__{}
+  @spec new(challenge :: String.t(), app_id :: String.t()) :: __MODULE__.t()
   def new(challenge, app_id) do
-    %__MODULE__{
+    struct!(
+      __MODULE__,
       challenge: challenge,
       app_id: app_id
-    }
+    )
   end
 
   @doc """
   Serializes a RegistrationRequest to binary so that the u2f device can read it.
   """
-  @spec to_binary(%__MODULE__{}) :: String.t()
+  @spec to_binary(__MODULE__.t()) :: String.t()
   def to_binary(%__MODULE__{challenge: challenge, app_id: app_id}) do
     Crypto.sha256(challenge) <> Crypto.sha256(app_id)
   end
@@ -30,7 +36,7 @@ defmodule U2FEx.RegistrationRequest do
   @doc """
   Serializes a RegistrationRequest to Json so that the javascript API can handle it.
   """
-  @spec to_json(%__MODULE__{}) :: String.t()
+  @spec to_json(__MODULE__.t()) :: String.t()
   def to_json(%__MODULE__{challenge: challenge, app_id: app_id}) do
     %{
       version: "U2F_V2",
