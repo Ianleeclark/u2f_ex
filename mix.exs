4dec6977e24d6473e7cc9f14123d9ffdc9e1734d
Ian Clark <ian@ianleeclark.com>
2018-11-24 23:49:09 -0800

    Cutting release for 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule U2fEx.MixProject do
   def project do
     [
       app: :u2f_ex,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
